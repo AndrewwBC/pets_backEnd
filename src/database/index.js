73019013fad2afc22d43888c570b3214c2d0ff23
AndrewwBC <andrew-hot2011@hotmail.com>
2024-02-17 14:37:48 -0300

    add: commenting db due by test

--- src/database/index.js
+++ src/database/index.js
@@ -1,18 +1,18 @@
-const { Client } = require("pg");
+// const { Client } = require("pg");
 
-const db = new Client({
-  host: "localhost",
-  user: "postgres",
-  password: "1234",
-  database: "pets",
-  port: 5432,
-});
+// const db = new Client({
+//   host: "localhost",
+//   user: "postgres",
+//   password: "1234",
+//   database: "pets",
+//   port: 5432,
+// });
 
-db.connect();
+// db.connect();
 
-async function ExecuteQuery(query, values) {
-  const { rows } = await db.query(query, values);
-  return rows;
-}
+// async function ExecuteQuery(query, values) {
+//   const { rows } = await db.query(query, values);
+//   return rows;
+// }
 
-module.exports = ExecuteQuery;
+// module.exports = ExecuteQuery;
